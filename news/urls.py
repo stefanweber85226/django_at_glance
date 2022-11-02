9982c997a62f6a06fb8f6bf9e95826b64ac14e88
daxtonkamitaira <daxton.kamitaira@gmail.com>
2022-11-02 04:15:06 +0100

    Set urls

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('articles/<int:year>/', views.year_archive),
-    path('articles/<int:year>/<int:month>/', views.month_archive),
-    path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
+    # path('articles/<int:year>/<int:month>/', views.month_archive),
+    # path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
 ]
